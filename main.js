751d80c25bf492dae32a3aa7b12a65274412fa75
hangju0610 <phj950610@naver.com>
2023-05-17 22:14:45 +0900

    Second day

--- main.js
+++ main.js
@@ -1,18 +1,85 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');  // url이란 모듈을 사용할 것이다. url이라는 변수를 통해서 사용할 것이다.
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(request.url, true).query; // querydate를 사용
+    var pathname = url.parse(_url, true).pathname;
+    var title = queryData.id
+
+    if (pathname === '/'){
+      if(queryData.id === undefined){
+
+        fs.readdir('./data', function(error,filelist){
+          var title = 'Welcome';
+          var description = 'hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i++;
+          }
+          
+          list = list + '</ul>';
+
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <h2>${title}</h2>
+            ${list}
+            <p>${description}</p>
+          </body>
+          </html>
+          `
+          response.writeHead(200); // 웹페이지에 원하는 데이터를 찾았을때 200
+          response.end(template); 
+        })
+
+      } else {
+        fs.readdir('./data', function(error,filelist){
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i++;
+          }
+          list = list + '</ul>';
+
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err,description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `
+          response.writeHead(200); // 웹페이지에 원하는 데이터를 찾았을때 200
+          response.end(template); 
+        });
+      });
     }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+  } else {
+    response.writeHead(404); // 404은 는 못찾았을때
+    response.end('Not found'); 
+
+  }
+
  
 });
 app.listen(3000);
---
+++ syntax/comparison.js
@@ -0,0 +1,6 @@
+console.log(1==1); //true 
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //false
+console.log(1===1); //true 정확하게 참과 거짓을 표현할때는 세개를 쓰자(피연산자의 값과 타입이 모두 같은 경우)
+console.log(1===2); //false 
